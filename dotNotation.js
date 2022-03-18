83449d39ca30ce4c9f598e52b84c5146ad99399b
Sayam Ajmal <itssanesayam@gmail.com>
2022-03-18 13:38:09 +0000

    2nd code

---
+++ dotNotation.js
@@ -0,0 +1,61 @@
+
+/* 1st try
+var columns =
+    '    |    |\n' +
+    '    |    |\n' +
+    '    |    |\n';
+
+var rows = 
+' ------------ ';
+
+var colNRows = columns + rows;
+
+
+//var rows ='-----------' ;
+
+console.log(colNRows);
+*/
+
+
+/* 2nd try - works
+
+
+let grid = ["   |   |   ", "   |   |   ", "   |   |   ", "-----------", "   |   |   ", "   |   |   ", "   |   |   ", "-----------", "   |   |   ", "   |   |   ", "   |   |   "]
+
+
+for (i = 0; i < grid.length; i++) {
+    console.log(grid[i])
+}
+
+*/
+
+
+
+
+
+
+// // #2 Using nested for loops ---gist 
+
+
+/*
+// This will loop code between here and * 3 times
+for (let i = 0; i<3; i++){
+    // This will loop code between here and ** 3 twice
+    for(let j = 0; j<2; j++){
+      // This will pring out twice because of the loop ^
+      console.log("    |    |    ")
+    }
+    // if i is NOT equal to 2 then print the below 
+    if (i !== 2){
+      console.log("--------------")
+    }
+  }
+  
+  console.log(Math.floor(Math.random()*10));
+*/
+
+
+
+
+
+
